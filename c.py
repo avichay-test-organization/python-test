0430ec9ecf806bbd095220699e325271c97f6754
avichay-attlan <107173654+avichay-attlan@users.noreply.github.com>
2022-07-07 10:32:03 +0300

    Update c.py

--- c.py
+++ c.py
@@ -8,3 +8,5 @@ a = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
 a = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
 a = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
 
+
+a = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
