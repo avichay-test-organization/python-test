a3ddae2eb2c406ceaca1b7983124d6fedff9e139
avichay-attlan <107173654+avichay-attlan@users.noreply.github.com>
2022-07-11 13:56:24 +0300

    Update c.py

--- c.py
+++ c.py
@@ -10,3 +10,15 @@ a = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
 
 
 a = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
+
+a = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
+a = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
+a = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
+a = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
+a = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
+a = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
+v
+a = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
+a = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
+a = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
+a = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
