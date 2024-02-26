dbbf477d0d4759d6ae33f47555f817f0fbb3f99e
avichay-attlan <107173654+avichay-attlan@users.noreply.github.com>
2024-02-26 11:18:51 +0200

    Update asdasd.py

--- asdasd.py
+++ asdasd.py
@@ -1,4 +1,54 @@
 
+aws_secret = "AKIAIMNOJVGFDXXXE4OB"
+
+
+
+
+
+assert eyal == 1
+
+another_var = "a"
+
+
+\sldkjflkdajglksdlkjsdg
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+a = "ASIAAQWSEDRFTGYHUJUJ"
+
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+
+
+
+
+
+
+
+
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+another_var = "a"
 a = 1
 
 
