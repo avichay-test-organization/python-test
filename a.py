81f067ed236b9e568725f3bd377d5b3c5c39eba9
avichay-attlan <107173654+avichay-attlan@users.noreply.github.com>
2024-03-10 10:58:20 +0200

    Update a.py

--- a.py
+++ a.py
@@ -5,6 +5,21 @@ d = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
 
 
 
+aws_secret = "AKIAIMNOJVGFDXXXE4OB"
+
+aws_secret = "AKIAIMNOJVGFDXXXE4OB"
+
+aws_secret = "AKIAIMNOJVGFDXXXE4OB"
+
+aws_secret = "AKIAIMNOJVGFDXXXE4OB"
+
+aws_secret = "AKIAIMNOJVGFDXXXE4OB"
+
+aws_secret = "AKIAIMNOJVGFDXXXE4OB"
+
+aws_secret = "AKIAIMNOJVGFDXXXE4OB"
+
+aws_secret = "AKIAIMNOJVGFDXXXE4OB"
 
 
 
